10e0b6fa6c30b0895ffb91617e03a15923546106
Everaldo Alexandre <everaldo_lol@hotmail.com>
2025-09-17 18:24:13 -0300

    add next-themes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ export default async function LoginPage() {
   }
 
   return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-gray-100 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -15,7 +15,7 @@ export default async function RegisterPage() {
   }
 
   return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-white flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
         <RegisterForm />
       </div>
